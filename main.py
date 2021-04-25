5a0f677edbc97e6d0e34fa12018aada1993cbab8
cpotipat <namsom.chitchanok@gmail.com>
2021-04-25 15:22:00 -0700

    Get internet speed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from selenium import webdriver
 import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 PROMISED_DOWN = 20
 PROMISED_UP = 60
@@ -11,11 +14,18 @@ TWITTER_PASSWORD = os.environ.get("TWITTER_PASSWORD")
 class InternetSpeedTwitterBot:
     def __init__(self, driver_path):
         self.driver = webdriver.Chrome(executable_path=driver_path)
-        self.up = 0
-        self.down = 0
+        self.upload = 0
+        self.download = 0
 
     def get_internet_speed(self):
-        pass
+        self.driver.get("https://www.speedtest.net/")
+        wait = WebDriverWait(self.driver, 180)
+
+        go_button = wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "start-text")))
+        go_button.click()
+        wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "result-container-meta")))
+        self.upload = self.driver.find_element_by_class_name("upload-speed").text
+        self.download = self.driver.find_element_by_class_name("download-speed").text
 
     def tweet_at_provider(self):
         pass
