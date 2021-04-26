1beb063896e1c73c93f521efd171f54518212e54
cpotipat <namsom.chitchanok@gmail.com>
2021-04-25 17:09:58 -0700

    Log in Twitter

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 PROMISED_DOWN = 20
 PROMISED_UP = 60
@@ -28,9 +29,19 @@ class InternetSpeedTwitterBot:
         self.download = self.driver.find_element_by_class_name("download-speed").text
 
     def tweet_at_provider(self):
-        pass
+        self.driver.get("https://twitter.com/login")
+        wait = WebDriverWait(self.driver, 180)
+
+        wait.until(EC.presence_of_element_located((By.NAME, "redirect_after_login")))
+        username_input = self.driver.find_element_by_name("session[username_or_email]")
+        username_input.clear()
+        username_input.send_keys(TWITTER_EMAIL)
+        password_input = self.driver.find_element_by_name("session[password]")
+        password_input.clear()
+        password_input.send_keys(TWITTER_PASSWORD)
+        password_input.send_keys(Keys.ENTER)
 
 
 bot = InternetSpeedTwitterBot(CHROME_DRIVER_PATH)
-bot.get_internet_speed()
+# bot.get_internet_speed()
 bot.tweet_at_provider()
